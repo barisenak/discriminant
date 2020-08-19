252b461a20878fd8d1d2a730e182a242ca07e75e
barisenak <mariya1999-7@mail.ru>
2020-08-19 23:43:36 +0300

    created with switch till the end

--- index.js
+++ index.js
@@ -1,35 +1,30 @@
 let a = prompt ("Введите -а-");
 let b = prompt ("Введите -b-");
 let c = prompt ("Введите -c-");
-let equality = alert (`Получили уравнение: ${a}x^2 + ${b}x + ${c} = 0`);
+let equality = alert (`Получили уравнение: ${a}x² + ${b}x + ${c} = 0`);
 let discriminant = Math.pow(b,2) - 4*a*c;
 isRoot(discriminant); 
 
 function isRoot (value){
-    // if (value > 0) {
-    //     alert (`Дискриминант данного уравнения равен ${discriminant} и имеет 2 корня`)
-    // } 
-    // if (value = 0) {
-    //     alert (`Дискриминант данного уравнения равен ${discriminant} и имеет 1 корень`)
-    // }
-
-    // if (value < 0) {
-    //     alert (`Дискриминант данного уравнения равен ${discriminant} и не имеет решений`)
-    // } else {
-    //     return false
-    // }
-
-    switch(value){
+    switch(true){
         case value > 0:
-            alert (`Дискриминант данного уравнения равен ${discriminant} и имеет 2 корня`)
+            alert (`Дискриминант данного уравнения равен ${discriminant} и имеет 2 корня`);
+            let x1 = (-b + Math.sqrt(discriminant))/2*a
+            let x2 = (-b - Math.sqrt(discriminant))/2*a
+            alert (`Решения уравнения:
+            x1 = ${x1}
+            x2 = ${x2}`)
             break
         case value === 0:
             alert (`Дискриминант данного уравнения равен ${discriminant} и имеет 1 корень`)
+            let x = -b/2*a
+            alert (`Решениe уравнения:
+            x = ${x}`)
             break
         case value < 0:
             alert (`Дискриминант данного уравнения равен ${discriminant} и не имеет решений`)
             break
         default: 
-        alert( "Нет таких значений" )
+        alert("Нет таких значений")
     }
 }
